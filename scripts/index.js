7e871ab009055b24c0c89b6962aff986d40e25f7
crecordz <s014451@mail.ru>
2023-04-21 21:39:35 +0300

    fix: pictures to change

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ const dropDown = document.querySelectorAll(".header__dropdown");
 const dropBtn = document.querySelectorAll(".header__dropbtn");
 const header = document.querySelector(".header");
 const arrows = document.querySelectorAll(".header__arrow");
+const dick = document.querySelector(".founders__dick");
+const depp = document.querySelector(".founders__depp");
 
 function showDropDown(event) {
   const currentDropDownContent = event.currentTarget.querySelector(
@@ -36,9 +38,23 @@ function closeDropDown(event) {
   });
 }
 
+function dickToLight() {
+  dick.classList.toggle("founders__dick_light");
+}
+
+function deppToLight() {
+  depp.classList.toggle("founders__depp_light");
+}
+
 dropDown.forEach((item) => {
   item.addEventListener("mouseover", showDropDown);
 });
 dropDown.forEach((item) => {
   item.addEventListener("mouseleave", closeDropDown);
 });
+
+dick.addEventListener("mouseover", dickToLight);
+dick.addEventListener("mouseleave", dickToLight);
+
+depp.addEventListener("mouseover", deppToLight);
+depp.addEventListener("mouseleave", deppToLight);
