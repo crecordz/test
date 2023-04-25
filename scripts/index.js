6c5f7dedf9bb63a7bb2074e00b38859589c8f1ce
crecordz <s014451@mail.ru>
2023-04-25 19:27:42 +0300

    feat:tubes

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,7 @@ const header = document.querySelector(".header");
 const arrows = document.querySelectorAll(".header__arrow");
 const dick = document.querySelector(".founders__dick");
 const depp = document.querySelector(".founders__depp");
+const tube = document.querySelector(".founders__flying-tube");
 
 function showDropDown(event) {
   const currentDropDownContent = event.currentTarget.querySelector(
@@ -22,6 +23,36 @@ function showDropDown(event) {
   if (currentArrow) {
     currentArrow.classList.add("header__arrow_rotate");
   }
+}
+
+const createFlyingTubeDepp = (offset) => {
+  setTimeout(() => {
+    const flyingTube = document.createElement("div");
+    flyingTube.classList.add("founders__flying-tube");
+    const maxLeft = depp.offsetWidth - flyingTube.offsetWidth;
+    const randomLeft = Math.floor(Math.random() * (maxLeft + 1));
+    flyingTube.style.left = `${randomLeft}px`;
+    depp.append(flyingTube);
+  }, offset);
+};
+
+for (let i = 0; i < 7; i++) {
+  createFlyingTubeDepp(i * 500);
+}
+
+const createFlyingTubeDick = (offset) => {
+  setTimeout(() => {
+    const flyingTube = document.createElement("div");
+    flyingTube.classList.add("founders__flying-tube");
+    const maxLeft = dick.offsetWidth - flyingTube.offsetWidth;
+    const randomLeft = Math.floor(Math.random() * (maxLeft + 1));
+    flyingTube.style.left = `${randomLeft}px`;
+    dick.append(flyingTube);
+  }, offset);
+};
+
+for (let i = 0; i < 7; i++) {
+  createFlyingTubeDick(i * 500);
 }
 
 function closeDropDown(event) {
